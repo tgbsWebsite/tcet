530cd7d16edbc2b199432ff6824164ef07e949db
tgbsWebsite <tgbsdigital@gmail.com>
2025-10-26 20:17:32 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,15 @@ import ProgrammesPage from "./Pages/ProgrammesPage";
 export default function App() {
   return (
     <Router>
-      {/* <Navbar /> */}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/programmespage" element={<ProgrammesPage />} />
-      </Routes>
-      {/* <Footer /> */}
+      <div className="bg-background-light dark:bg-background-dark font-display text-gray-800 dark:text-gray-200 min-h-screen">
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/programmespage" element={<ProgrammesPage />} />
+        </Routes>
+        {/* <Footer /> */}
+      </div>
     </Router>
   );
 }
