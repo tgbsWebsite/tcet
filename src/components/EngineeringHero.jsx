cdc4ee49b36bbaa2067e6cff364b438d4bbb7ba3
tgbsWebsite <tgbsdigital@gmail.com>
2025-10-27 13:04:32 +0530

    Add files via upload

--- src/components/EngineeringHero.jsx
+++ src/components/EngineeringHero.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-import "./engv-hero.css";
+import "./engineering-hero.css";
 
 const carouselData = [
   {
